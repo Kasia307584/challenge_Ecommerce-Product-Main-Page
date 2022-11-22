8d559bba7843904b5342f2c0278563135e72a753
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2022-11-22 15:31:35 +0100

    Basket and button work fine, but code need refactoring (repetitions)

--- main.js
+++ main.js
@@ -11,9 +11,6 @@ const productPrice = document.querySelector(".product-price");
 const cart = document.querySelector(".cart-content");
 const articlesInBasket = document.querySelector(".basket-icon");
 const basketIcon = document.querySelector(".basket-icon > img");
-// const productPriceCart = document.querySelector(".cart-product-price");
-// const productCountCart = document.querySelector(".cart-product-count");
-// const productSumCart = document.querySelector(".cart-product-sum");
 
 plusIcon.addEventListener("click", () => {
   productCount.textContent = counter.addCount();
@@ -30,9 +27,33 @@ btnAdd.addEventListener("click", () => {
   if (countValue > 0) {
     articlesInBasket.classList.add("bullet-icon");
     articlesInBasket.setAttribute("value", countValue);
+    cart.innerHTML = `<h3>Cart</h3>
+        <hr /><div class="cart-info-wrapper">
+      <img
+        class="cart-photo"
+        src="images/image-product-1-thumbnail.jpg"
+        alt="Product image"
+      />
+      <span class="cart-product-name">Fall Limited Edition Sneakers</span>
+      <span class="cart-product-price">${productPrice.textContent}</span
+      ><span class="cart-product-count"> x${countValue} </span
+      ><span class="cart-product-sum">$${counter.sumToDisplay(
+        getPrice(productPrice)
+      )}.00</span>
+      <i class="fa-solid fa-trash-can"></i>
+    </div>
+    <div class="button-box">
+      <button class="btn btn-checkout" type="button">Checkout</button>
+    </div>`;
   } else {
     articlesInBasket.classList.remove("bullet-icon");
+    cart.innerHTML = `<h3>Cart</h3><hr class="for-empty-cart"><div class="cart-message">Your cart is empty.</div>`;
   }
+  const deleter = document.querySelector(".fa-solid");
+
+  deleter.addEventListener("click", () => {
+    cart.innerHTML = `<h3>Cart</h3><hr class="for-empty-cart"><div class="cart-message">Your cart is empty.</div>`;
+  });
 });
 
 basketIcon.addEventListener("click", () => {
@@ -60,10 +81,9 @@ basketIcon.addEventListener("click", () => {
       <button class="btn btn-checkout" type="button">Checkout</button>
     </div>`;
   }
+  const deleter = document.querySelector(".fa-solid");
+
+  deleter.addEventListener("click", () => {
+    cart.innerHTML = `<h3>Cart</h3><hr class="for-empty-cart"><div class="cart-message">Your cart is empty.</div>`;
+  });
 });
-
-// const deleter = document.querySelector(".fa-solid");
-
-// deleter.addEventListener("click", () => {
-//   cart.innerHTML = `<h3>Cart</h3><hr class="for-empty-cart"><div class="cart-message">Your cart is empty.</div>`;
-// });
